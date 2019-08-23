7fe52421d2422418ef3b84394881216e1918eb4b
vunderkind <justin.irabor@gmail.com>
2019-08-23 08:25:55 +0100

    Starting out on objects-arrays.js, pushing this to share PR with Martin

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,29 +6,51 @@
   object name, diet, weight, length, period
 */
 
-// tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+let tyrannosaurus = {
+  name:"tyrannosaurus",
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous'
+};
 
-// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+let stegosaurus = {
+  name:"stegosaurus",
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+};
 
-// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+let velociraptor = {
+  name:"velociraptor",
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+};
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tyrannosaurus.roar = function() {
+  return "RAWERSRARARWERSARARARRRR!";
+}
+
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -46,8 +68,14 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
+/* Request 1: Create a new array called universities that contains all the universities in the graduates array. 
+*/ 
+let universities = [];
+graduates.forEach = function(universities) {
+  universities.push(graduates[universities.university])
+}
 
+/*
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
 console.log(universities)
